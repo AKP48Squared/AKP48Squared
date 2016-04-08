dc0ab95b057f9d868fed79b8dc8e0f13c70453b6
Austin Peterson <austin@akpwebdesign.com>
2016-04-08 14:08:42 -0500

    Add Trello command.

---
+++ plugins/basic-commands/commands/trello.js
@@ -0,0 +1,9 @@
+function Trello() {
+  this.names = ['trello', 'issues'];
+}
+
+Trello.prototype.respond = function () {
+  return 'https://trello.com/b/ZfENFVny/akp48';
+};
+
+module.exports = Trello;
