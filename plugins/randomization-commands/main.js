a72b082e3216914bd45175632e34ec3ab6b0819f
Austin Peterson <austin@akpwebdesign.com>
2016-04-12 10:07:16 -0500

    Quick attempt at migrating over the roll command, into a new plugin dedicated to randomization commands.

---
+++ plugins/randomization-commands/main.js
@@ -0,0 +1,28 @@
+'use strict';
+// const MessageHandlerPlugin = require('../../lib/MessageHandlerPlugin');
+const BasicCommands = require('../basic-commands/main.js');
+
+class RandomizationCommands extends BasicCommands {
+  constructor(AKP48) {
+    super(AKP48);
+    //manually override pluginName, since we're extending another plugin.
+    this._pluginName = 'RandomizationCommands';
+    var self = this;
+    this.commands = {};
+    require('./commands').then(function(res){
+      self.commands = res;
+    }, function(err){
+      console.error(err);
+    });
+  }
+
+  handleCommand(message, context) {
+    GLOBAL.logger.silly(`${this._pluginName}: Received command.`);
+
+    //run the handleCommand logic from BasicCommands, which should use our defined commands instead.
+    GLOBAL.logger.silly(`${this._pluginName}: Attempting to handle command using BasicCommands logic.`);
+    super.handleCommand(message, context);
+  }
+}
+
+module.exports = RandomizationCommands;
