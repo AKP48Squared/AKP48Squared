857804aa710810f9281fce3ddc47cee831c93e5f
Austin Peterson <austin@akpwebdesign.com>
2016-04-12 11:03:24 -0500

    add message to shutdown command.

--- plugins/basic-commands/commands/shutdown.js
+++ plugins/basic-commands/commands/shutdown.js
@@ -4,7 +4,7 @@ function Shutdown() {
 
 Shutdown.prototype.respond = function (context) {
   if(context.perms && context.perms.contains('basic-commands.shutdown') || true) {
-    GLOBAL.AKP48.shutdown();
+    GLOBAL.AKP48.shutdown(context.text);
   }
   return '';
 };
