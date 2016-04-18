3ee9e98e2948fe951bf50be2edb5aef6f427e45b
Austin Peterson <austin@akpwebdesign.com>
2016-04-18 13:11:39 -0500

    Conversion: Add more conversions. Lots more.

--- plugins/conversion-commands/main.js
+++ plugins/conversion-commands/main.js
@@ -2,6 +2,7 @@
 const ConvertBase = require('./ConvertBase');
 const ConvertTemp = require('./ConvertTemp');
 const MessageHandlerPlugin = require('../../lib/MessageHandlerPlugin');
+const Qty = require('js-quantities');
 
 class Conversion extends MessageHandlerPlugin {
   constructor(AKP48) {
@@ -18,10 +19,10 @@ Conversion.prototype.handleCommand = function (message, context) {
   var command = text[0];
   //text is now an array of all the arguments the user sent.
   text.shift();
+  var responses = [];
 
   if(typeof ConvertBase[command] === 'function') {
     GLOBAL.logger.silly(`${this._pluginName}: Responding to ${command} command.`);
-    var responses = [];
     for (var i = 0; i < text.length; i++) {
       responses.push(`${text[i]} => ${ConvertBase[command](text[i])}`);
     }
@@ -31,10 +32,29 @@ Conversion.prototype.handleCommand = function (message, context) {
 
   if(typeof ConvertTemp[command] === 'function') {
     GLOBAL.logger.silly(`${this._pluginName}: Responding to ${command} command.`);
-    var responses = [];
     for (var i = 0; i < text.length; i++) {
       responses.push(`${ConvertTemp[command](text[i])}`);
     }
+    this._AKP48.sendMessage(context.instanceId, context.to, responses.join(', '), context);
+  }
+
+  //all-in-one solution time.
+  if(command === 'convert') {
+    var to = text[0];
+    text.shift();
+    try {
+      for (var i = 0; i < text.length; i++) {
+        var q = new Qty(text[i]);
+        if(q.isCompatible(to)) {
+          responses.push(`${q.toPrec(0.01).toString()} => ${q.to(to).toPrec(0.01).toString()}`);
+        } else {
+          responses.push(`Incompatible units: ${q.toPrec(0.01).toString()}`);
+        }
+      }
+    } catch(e) {
+      responses.push(`Error! ${e.message}.`);
+    }
+
     this._AKP48.sendMessage(context.instanceId, context.to, responses.join(', '), context);
   }
 };
