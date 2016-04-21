898536ee4dedbe300e8e0933d7befe68035f3a90
Austin Peterson <austin@akpwebdesign.com>
2016-04-20 22:27:24 -0500

    Remove child_process code, move reloading up to app.js.

--- app.js
+++ app.js
@@ -1,26 +1,37 @@
-require('./lib/polyfill'); //load polyfill
-var AKP48 = require('./lib/AKP48');
-var config;
-var logger;
+function load(persistObjs) {
+  require('./lib/polyfill'); //load polyfill
+  var AKP48 = require('./lib/AKP48');
+  var config;
+  var logger;
 
-try {
-  config = require('./config.json');
-} catch(e) {
-  console.log('No config file found.');
-  //no config, so set config to null.
-  config = null;
+  try {
+    config = require('./config.json');
+  } catch(e) {
+    console.log('No config file found.');
+    //no config, so set config to null.
+    config = null;
+  }
+
+  try {
+    logger = require('./lib/Logger')(config.logger.level || 'info');
+  } catch(e) {
+    logger = require('./lib/Logger')('info');
+  }
+
+  logger.info('AKP48 is starting.');
+
+  //logger goes in global scope.
+  GLOBAL.logger = logger;
+
+  //load the bot.
+  GLOBAL.AKP48 = new AKP48(config, reload, persistObjs);
 }
 
-try {
-  logger = require('./lib/Logger')(config.logger.level || 'info');
-} catch(e) {
-  logger = require('./lib/Logger')('info');
+function reload(persistObjs) {
+  delete GLOBAL.AKP48;
+  GLOBAL.logger.info('Reloading AKP48.');
+  delete GLOBAL.logger;
+  load(persistObjs);
 }
 
-logger.info('AKP48 is starting.');
-
-//logger goes in global scope.
-GLOBAL.logger = logger;
-
-//load the bot.
-GLOBAL.AKP48 = new AKP48(config, process.env.AKP48Restart);  
+load();
