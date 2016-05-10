430b7b7aaa02081a71e491d4ea12158182b61370
Austin Peterson <austin@akpwebdesign.com>
2016-05-10 13:38:37 -0500

    add command line params for config.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ function load(persistObjs, startTime) {
   var logger;
 
   try {
-    config = require('./config.json');
+    config = require(getConfigFile());
   } catch(e) {
     console.log('No config file found.');
     //no config, so set config to null.
@@ -30,7 +30,7 @@ function load(persistObjs, startTime) {
   global.logger = logger;
 
   //load the bot.
-  global.AKP48 = new AKP48(config, reload, persistObjs, startTime);
+  global.AKP48 = new AKP48(config, getConfigDir(), reload, persistObjs, startTime);
 }
 
 function reload(persistObjs, startTime) {
@@ -38,6 +38,41 @@ function reload(persistObjs, startTime) {
   global.logger.info('Reloading AKP48.');
   delete global.logger;
   load(persistObjs, startTime);
+}
+
+function getConfigDir() {
+  //argv contains a nice object with our arguments. See https://www.npmjs.com/package/minimist for more details.
+  var argv = require('minimist')(process.argv.slice(2));
+  var path = require('path');
+  var homedir = require('homedir');
+
+  var conf = argv.config || argv.conf;
+
+  if(conf) {
+    return path.resolve(path.dirname(conf));
+  }
+
+  switch(process.platform) {
+    case 'darwin':
+      return path.join(process.env.HOME, 'Library/Application Support/AKP48Squared');
+    case 'win32':
+      return path.join(process.env.APPDATA, 'AKP48Squared');
+    default:
+      return path.join(homedir(), '.akp48squared');
+  }
+}
+
+function getConfigFile() {
+  //argv contains a nice object with our arguments. See https://www.npmjs.com/package/minimist for more details.
+  var argv = require('minimist')(process.argv.slice(2));
+  var path = require('path');
+  var conf = argv.config || argv.conf;
+
+  if(conf) {
+    return path.resolve(conf);
+  }
+
+  return path.resolve(getConfigDir(), 'config.json');
 }
 
 load();
